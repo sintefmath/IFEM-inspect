f9e4657ad69eaae9fe602da4a284f7ec87e3e32c
Eivind Fonn <evfonn@gmail.com>
2016-02-29 11:14:07 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='IFEM-Inspect',
+    version='0.0.1',
+    description='Inspect IFEM result files',
+    author='Eivind Fonn',
+    author_email='eivind.fonn@sintef.no',
+    scripts=['ifem-inspect'],
+)
